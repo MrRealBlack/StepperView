c7bb8c1fe5ce293e5915b69b421dda7475d27973
Mehdi Gilanpour <>
2022-02-19 13:06:57 +0330

    Package.swift updated 🚀

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 let package = Package(
 
     name: "StepperView",
-    platforms: [
-          .iOS(.v13)
-        ],
     products: [
         .library(name: "StepperView",   targets: ["StepperView"]),
     ],
